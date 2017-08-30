f45756f2ab6c8cc387baf3a1d5db33a1bbdc5477
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-30 20:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,16 +49,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var counter = 0
 
   for (var i = 0; i < cart.length; i++) {
     if (Object.keys(cart[i]) == item) {
       cart.splice(i, 1);
       counter += 1;
-    } else if (counter == 0) {
-      console.log("That item is not in your cart.");
-    }
-    console.log('That item is not in your cart.')
+      break;
+    } 
   }
   console.log("That item is not in your cart.");
 }
