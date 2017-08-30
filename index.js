604c59b5d5c72b3ecb65d66dc96140e7210a2c43
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-30 17:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,5 +52,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    
+  }
 }
