dafeebf6ae1689470fedf1714cf9e5bec2975c22
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-30 20:12:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
       for (var i = 0; i < cart.length - 1; i++) {
          cartItems.push(`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
       }
-        console.log(`In your cart, you have ${cartItems.join(', ')} and ${Object.keys(cart[cart.length - 1])} at $${cart[cart.length - 1][Object.keys(cart[cart.length - 1])]}.`);
+        console.log(`In your cart, you have ${cartItems.join(', ')}, and ${Object.keys(cart[cart.length - 1])} at $${cart[cart.length - 1][Object.keys(cart[cart.length - 1])]}.`);
     } else {
         console.log('Your shopping cart is empty.');
   }
