c45a03f12d8792a6411ddb79f6891904a7421352
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-30 17:29:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,10 +44,9 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
-      i++
     } else {
       return "That item is not in your cart."
-      break;
+      ;break;
     }
     return cart
   }
