b708babad4dfc3154abed89c29977261123c8ab1
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-30 19:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function getRandomInt(min, max) {
 function addToCart(item) {
  cart.push({[item]: getRandomInt(0, 100)});
  console.log(`${item} has been added to your cart.`)
- return cart
+console.log(cart);
 }
 
 function viewCart() {
