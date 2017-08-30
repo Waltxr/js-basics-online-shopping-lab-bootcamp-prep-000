2afc0121be32d6587f6b5cbb4aee348c8d298d83
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-30 20:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
 
 function removeFromCart(item) {
   var counter = 0
-  
+
   for (var i = 0; i < cart.length; i++) {
     if (Object.keys(cart[i]) == item) {
       cart.splice(i, 1);
